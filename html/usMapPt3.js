d5a268651dd7b213467ca71777b25269bf1669b0
dmoreno757 <dmoreno757@gmail.com>
2022-07-17 22:37:44 -0700

    pt2

--- html/usMapPt3.js
+++ html/usMapPt3.js
@@ -1,30 +1,61 @@
-(function () {
-var margin = {top: 0, right: 0, bottom: 0, left: 0},
-    width = 900 - margin.left - margin.right,
-    height = 800 - margin.top - margin.bottom;
+var countyLink = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
+var countyData;
 
-// append the svg object to the body of the page
-var svg = d3.select("#usMap")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var countyStatLink = 'https://data.cdc.gov/resource/3nnm-4jni.json';
+var countyStatData;
 
-d3.queue()
-    .defer(d3.json, "us.json")
-    .await(mapping)
+map = d3.select('#usMap')
 
-var projection = d3.geoMercator()
-    .translate([width/2, height/2])
-    .scale(100)
-
-var path = d3.geoPath()
-    .projection(projection)
-
-
-function mapping(data) {
-    console.log(data)
+var drawMap = () => {
+    map.selectAll("path")
+       .data(countyData)
+       .enter()
+       .append("path")
+       .attr("d", d3.geoPath())
+       .attr('class', 'county')
+       .attr('fill', (item) => {
+        var fips = item['id']
+        var county = countyStatData.find((county) => {
+            //console.log(county['fips'])
+            return county['county_fips'] == fips
+        })
+        console.log(county)
+        function funcTest(cases) {
+            if (cases === undefined) {
+                console.log(cases.not)
+            }
+            return cases;
+        }
+        
+        var cases = county['covid_cases_per_100k'];
+        console.log(funcTest(cases));
+      })
 }
-});
+
+
+d3.json(countyLink).then(
+    (data, err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            countyData = data
+            countyData = topojson.feature(countyData, countyData.objects.counties).features
+            console.log('countydata!')
+            console.log(countyData)
+
+            d3.json(countyStatLink).then(
+                (data, err) => {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        countyStatData = data
+                        console.log("COUNTY STAT DATA")
+                        console.log(countyStatData)
+                        drawMap()
+                    }
+                }
+            )
+        }
+    }
+)
 
