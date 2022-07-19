a35ab3c9ee2223bc92fd0e8972eda8e9243e7c69
dmoreno757 <dmoreno757@gmail.com>
2022-07-18 20:15:38 -0700

    p3

--- html/usMapPt3.js
+++ html/usMapPt3.js
@@ -1,10 +1,22 @@
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 1100 - margin.left - margin.right,
+    height = 750 - margin.top - margin.bottom;
+  
+
+//https://www.notion.so/Visualize-Data-with-a-Choropleth-Map-9d91d46e78d4406abc6a0d36f9e089dc
 var countyLink = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
 var countyData;
 
-var countyStatLink = 'https://data.cdc.gov/resource/3nnm-4jni.json';
+//https://data.cdc.gov/Public-Health-Surveillance/United-States-COVID-19-Community-Levels-by-County/3nnm-4jni/data
+var countyStatLink = 'https://raw.githubusercontent.com/dmoreno757/dmoreno757.github.io/main/html/United_States_COVID-19_Community_Levels_by_County1.csv';
 var countyStatData;
 
 map = d3.select('#usMap')
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
 var drawMap = () => {
     map.selectAll("path")
@@ -16,19 +28,36 @@ var drawMap = () => {
        .attr('fill', (item) => {
         var fips = item['id']
         var county = countyStatData.find((county) => {
-            //console.log(county['fips'])
             return county['county_fips'] == fips
         })
-        console.log(county)
-        function funcTest(cases) {
-            if (cases === undefined) {
-                console.log(cases.not)
-            }
-            return cases;
-        }
-        
         var cases = county['covid_cases_per_100k'];
-        console.log(funcTest(cases));
+        console.log(cases)
+        if (cases <= 100){
+            return '#feeceb'
+        }else if (cases <= 500){
+            return '#fdd9d7'
+        } else if (cases <= 1000){
+            return '#fcc7c3'
+        } else if (cases <= 1500){
+            return '#fbb4af'
+        } else if (cases <= 2000){
+            return '#faa19b'
+        } else if (cases <= 2500){
+            return '#f88e86'
+        } else if (cases <= 3000){
+            return '#f77b72'
+        } else if (cases <= 3500){
+            return '#f6695e'
+        } else if (cases <= 4000){
+            return '#f5564a'
+        } else if (cases <= 4500){
+            return '#f5564a'
+        } else if (cases <= 5000){
+            return '#f44336'
+        }
+        else {
+            return 'black'
+        }
       })
 }
 
@@ -43,7 +72,7 @@ d3.json(countyLink).then(
             console.log('countydata!')
             console.log(countyData)
 
-            d3.json(countyStatLink).then(
+            d3.csv(countyStatLink).then(
                 (data, err) => {
                     if (err) {
                         console.log(err)
